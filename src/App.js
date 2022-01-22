daf63c05b070e4e8e12c7c25c95f1d920b3a27cc
garlife <garlife@gmail.com>
2022-01-22 10:50:58 +0300

    array function + map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+const users = [
+  {
+    name: 'Alex',
+    email: 'Alex@sberbank.ru',
+  },
+  {
+    name: 'Yan',
+    email: 'Yan@sberbank.ru',
+  },
+  {
+    name: 'Max',
+    email: 'Max@sberbank.ru',
+  },
+];
+
+// const Exapmle1 = () => (
+// <div>Hello, world!</div>
+// );
+
+// const Exapmle2 = () => <div>{"Hello, world!"}</div>;
+
+// const helloWorld = 'Hello, world!';
+// const Exapmle3 = () => {
+//   return <div>{helloWorld}</div>;
+// };
+
+const Userlist = ({ users }) => (
+  <ul>
+    {users.map(({ name, email, index }) => (
+      <>
+        <li key={index}>
+          {' '}
+          {name} {'<'}
+          {email}
+          {'>'};
+        </li>
+      </>
+    ))}
+  </ul>
+);
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <Userlist users={users} />
+    </>
   );
 }
